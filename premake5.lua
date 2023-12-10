0f6a629ffb19117d0c82f74b57eb8153c0171b2b
Joel Watson <admin@matriko.xyz>
2023-12-10 18:11:33 +1000

    tailored to my needs

--- premake5.lua
+++ premake5.lua
@@ -8,25 +8,33 @@ project "GLFW"
 	files
 	{
         "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
     }
     
 	filter "system:windows"
         buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "10.0.17134.0"
+        systemversion "latest"
         staticruntime "On"
         
         files
         {
             "src/win32_init.c",
             "src/win32_joystick.c",
+            "src/win32_module.c",
             "src/win32_monitor.c",
             "src/win32_time.c",
             "src/win32_thread.c",
@@ -41,5 +49,16 @@ project "GLFW"
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
 		}
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+    filter "configurations:Debug"
+    		runtime "Debug"
+    		symbols "on"
+
+	filter "configurations:Beta"
+		runtime "Release"
+		optimize "speed"
+
+    filter "configurations:Release"
+		runtime "Release"
+		optimize "speed"
+        symbols "off"
